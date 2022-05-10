57754e1fe944376de8894d8d390eacc96e02d433
Evan Mullins <evanrm@gmail.com>
2022-05-10 16:39:15 -0700

    add tabs for organization and some more sample data

--- components/marketplace/index.js
+++ components/marketplace/index.js
@@ -5,15 +5,39 @@
  * @param {*} props 
  * @returns 
  */
- const Marketplace = ({useState, useEffect, ...props}) => {
-    const [ isLoading, setIsLoading ] = useState( true );
+ const Marketplace = ({methods, constants, Components, ...props}) => {
+    const [ isLoading, setIsLoading ] = methods.useState( true );
 	// const [ marketplaceTypes, setMarketplaceTypes ] = useState( [] );
-    const [ marketplaceItems, setMarketplaceItems ] = useState( [] );
+    const [ marketplaceItems, setMarketplaceItems ] = methods.useState( [] );
+
+	const [ initialTab, setInitialTab ] = methods.useState( 'all' );
+	// const location = methods.useLocation();
+	// const navigate = useNavigate();
+
+	const onTabNavigate = ( tabName ) => {
+		// navigate( '/marketplace/' + tabName, { replace: true } );
+        console.log(tabName);
+    };
+
+	// useEffect( () => {
+	// 	if ( location.pathname.includes( '/services' ) ) {
+	// 		setInitialTab( 'services' );
+	// 	} else if ( location.pathname.includes( '/themes' ) ) {
+	// 		setInitialTab( 'themes' );
+	// 	} else if ( ! location.pathname.includes( '/plugins' ) ) {
+	// 		navigate( '/marketplace/plugins', { replace: true } );
+	// 	}
+	// 	setIsLoading( false );
+	// }, [ location ] );
+
+	// if ( isLoading || ! marketplaceItems.length < 0 ) {
+	// 	return <Components.Spinner />;
+	// }
     
     // on mount load all marketplace data from module api
-    useEffect(() => {
-        props.apiFetch( {
-            url: `${props.resturl}/newfold-marketplace/v1/marketplace`
+    methods.useEffect(() => {
+        methods.apiFetch( {
+            url: `${constants.resturl}/newfold-marketplace/v1/marketplace`
         }).then( ( response ) => {
             setIsLoading(false);
             setMarketplaceItems(response); // all products
@@ -22,54 +46,91 @@
 	}, [] );
 
     return (
-        <div className={props.classnames('newfold-marketplace-wrapper')}>
-            <h1>Marketplace</h1>
-            <div className="grid col2">
-                {marketplaceItems.map((item) => (
-                    <MarketplaceItem 
-                        key={item.hash} 
-                        item={item}
-                        Card={props.Card}
-                        CardHeader={props.CardHeader}
-                        CardFooter={props.CardFooter}
-                        CardMedia={props.CardMedia}
-                        CardBody={props.CardBody}
-                        Button={props.Button}
-                    />
-                ))}
-            </div>
+        <div className={methods.classnames('newfold-marketplace-wrapper')}>
+            <Components.TabPanel
+				className="newfold-marketplace-tabs"
+				activeClass="current-tab"
+				orientation="vertical"
+				initialTabName={ initialTab }
+				onSelect={ onTabNavigate }
+				tabs={ [
+					{
+						name: 'all',
+						className: 'all',
+						title: 'Everything'
+					},
+					{
+						name: 'plugin',
+						className: 'plugins',
+						title: 'Plugins'
+					},
+					{
+						name: 'service',
+						className: 'services',
+						title: 'Services'
+					},
+					{
+						name: 'theme',
+						className: 'themes',
+						title: 'Themes'
+					},
+				] }
+			>
+				{ ( tab ) => <MarketplaceList
+                    marketplaceItems={marketplaceItems}
+                    type={tab.name}
+                    Components={Components}
+                /> }
+			</Components.TabPanel>
         </div>
     )
 
 };
 
-const MarketplaceItem = ({ item, Card, CardMedia, CardHeader, CardBody, CardFooter, Button }) => {
+const MarketplaceList = ({ marketplaceItems, type = 'all', Components }) => {
+    return (
+        <div className="grid col2">
+            {marketplaceItems.filter((item) => {
+                return type === 'all' || item.type === type
+            }).map((item) => (
+                <MarketplaceItem 
+                    type={type}
+                    key={item.hash} 
+                    item={item}
+                    Components={Components}
+                />
+            ))}
+        </div>
+    )
+};
+
+const MarketplaceItem = ({ item, Components }) => {
 
     return (
-        <Card className={ `marketplace-item marketplace-item-${ item.hash }` }>
+        <Components.Card className={ `marketplace-item marketplace-item-${ item.hash }` }>
 			{ item.productThumbnailUrl && (
-				<CardMedia>
+				<Components.CardMedia>
 					<img src={ item.productThumbnailUrl } alt={ item.name + ' thumbnail' } />
-				</CardMedia>
+				</Components.CardMedia>
 			) }
-			<CardHeader>
+			<Components.CardHeader>
 				<h3>{ item.name }</h3>
 				{ item.price && <em className="price">{ item.price }</em> }
-			</CardHeader>
-			{ item.description && <CardBody>{ item.description }</CardBody> }
-			<CardFooter>
+			</Components.CardHeader>
+			{ item.description && <Components.CardBody>{ item.description }</Components.CardBody> }
+			<Components.CardFooter>
                 { item.primaryCallToAction && item.primaryUrl &&
-                    <Button variant="primary" href={ item.primaryUrl }>
+                    <Components.Button variant="primary" href={ item.primaryUrl }>
                         { item.primaryCallToAction }
-                    </Button>
+                    </Components.Button>
                 }
                 { item.secondaryCallToAction && item.secondaryUrl &&
-                    <Button variant="secondary" href={ item.secondaryUrl }>
+                    <Components.Button variant="secondary" href={ item.secondaryUrl }>
                         { item.secondaryCallToAction }
-                    </Button>
+                    </Components.Button>
                 }
-			</CardFooter>
-		</Card>
+			</Components.CardFooter>
+		</Components.Card>
     );
 };
 
