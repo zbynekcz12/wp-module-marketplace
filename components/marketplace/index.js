4c6bfe4e20e8c521cfabf65e9021dd5e7ea23c2f
Evan Mullins <evanrm@gmail.com>
2022-07-01 15:22:03 -0400

    convert indentation to tabs

--- components/marketplace/index.js
+++ components/marketplace/index.js
@@ -8,10 +8,10 @@ import { default as MarketplaceList } from '../marketplaceList/';
  * @returns 
  */
  const Marketplace = ({methods, constants, Components, ...props}) => {
-    const [ isLoading, setIsLoading ] = methods.useState( true );
-    const [ isError, setIsError ] = methods.useState( false );
+	const [ isLoading, setIsLoading ] = methods.useState( true );
+	const [ isError, setIsError ] = methods.useState( false );
 	const [ marketplaceCategories, setMarketplaceCategories ] = methods.useState( [] );
-    const [ marketplaceItems, setMarketplaceItems ] = methods.useState( [] );
+	const [ marketplaceItems, setMarketplaceItems ] = methods.useState( [] );
 	const [ initialTab, setInitialTab ] = methods.useState( 'Featured' );
 
 	// const location = methods.useLocation();
@@ -19,8 +19,8 @@ import { default as MarketplaceList } from '../marketplaceList/';
 
 	const onTabNavigate = ( tabName ) => {
 		// methods.navigate( '/marketplace/' + tabName, { replace: true } );
-        // console.log( tabName );
-    };
+		// console.log( tabName );
+	};
 
 	// useEffect( () => {
 	// 	if ( location.pathname.includes( '/services' ) ) {
@@ -32,38 +32,38 @@ import { default as MarketplaceList } from '../marketplaceList/';
 	// 	}
 	// 	setIsLoading( false );
 	// }, [ location ] );
-    
-    /**
-     * on mount load all marketplace data from module api
-     */
-    methods.useEffect(() => {
-        methods.apiFetch( {
-            url: `${constants.resturl}/newfold-marketplace/v1/marketplace`
-        }).then( ( response ) => {
-            // console.log(response);
-            setIsLoading( false );
-            // check response for data
-            if ( ! response.hasOwnProperty('data') ) {
-                setIsError( true );
-            } else {
-                const products = response['data'];
-                setMarketplaceItems( products );
-                setMarketplaceCategories( collectCategories( products ) );
-            }
+	
+	/**
+	 * on mount load all marketplace data from module api
+	 */
+	methods.useEffect(() => {
+		methods.apiFetch( {
+			url: `${constants.resturl}/newfold-marketplace/v1/marketplace`
+		}).then( ( response ) => {
+			// console.log(response);
+			setIsLoading( false );
+			// check response for data
+			if ( ! response.hasOwnProperty('data') ) {
+				setIsError( true );
+			} else {
+				const products = response['data'];
+				setMarketplaceItems( products );
+				setMarketplaceCategories( collectCategories( products ) );
+			}
 		});
 	}, [] );
 
-    /**
-     * map all categories into an array for consuming by tabpanel component
-     * @param Array products 
-     * @returns 
-     */
-    const collectCategories = ( products ) => {
-        
-        if ( ! products.length ) {
-            return;
-        }
-        
+	/**
+	 * map all categories into an array for consuming by tabpanel component
+	 * @param Array products 
+	 * @returns 
+	 */
+	const collectCategories = ( products ) => {
+		
+		if ( ! products.length ) {
+			return;
+		}
+		
 		let thecategories = [];
 		let cats = new Set();
 		products.forEach((product) => {
@@ -75,66 +75,66 @@ import { default as MarketplaceList } from '../marketplaceList/';
 			thecategories.push( {
 				name: cat,
 				title: cat,
-                currentCount: constants.perPage
+				currentCount: constants.perPage
 			});
 		});
-        thecategories.sort(
+		thecategories.sort(
 			// sort alphabetically, but stick featured to top
-            function( a, b ) {
-                if(a.name.toLowerCase() === 'featured' || a.name.toLowerCase() < b.name.toLowerCase()) { return -1; }
-                if(b.name.toLowerCase() === 'featured' || a.name.toLowerCase() > b.name.toLowerCase()) { return 1; }
-                return 0;
-            }
-        );
+			function( a, b ) {
+				if(a.name.toLowerCase() === 'featured' || a.name.toLowerCase() < b.name.toLowerCase()) { return -1; }
+				if(b.name.toLowerCase() === 'featured' || a.name.toLowerCase() > b.name.toLowerCase()) { return 1; }
+				return 0;
+			}
+		);
 		return thecategories;
-    };
+	};
 
-    /**
-     * Save a potential updated display counts per category
-     * @param string categoryName 
-     * @param Number newCount 
-     */
-    const saveCategoryDisplayCount = (categoryName, newCount) => {
-        let updatedMarketplaceCategories = [...marketplaceCategories];
-        // find matching cat, and update perPage amount
-        updatedMarketplaceCategories.forEach((cat)=>{
-            if (cat.name === categoryName ) {
-                cat.currentCount = newCount;
-            }
-        });
-        setMarketplaceCategories( updatedMarketplaceCategories );
-    };
+	/**
+	 * Save a potential updated display counts per category
+	 * @param string categoryName 
+	 * @param Number newCount 
+	 */
+	const saveCategoryDisplayCount = (categoryName, newCount) => {
+		let updatedMarketplaceCategories = [...marketplaceCategories];
+		// find matching cat, and update perPage amount
+		updatedMarketplaceCategories.forEach((cat)=>{
+			if (cat.name === categoryName ) {
+				cat.currentCount = newCount;
+			}
+		});
+		setMarketplaceCategories( updatedMarketplaceCategories );
+	};
 
-    return (
-        <div className={methods.classnames('newfold-marketplace-wrapper')}>
-            { isLoading && 
-                <Components.Spinner />
-            }
-            { isError && 
-                <h3>Oops, we encountered an error loading the marketplace, please try again later.</h3>
-            }
-            { !isLoading && !isError &&
-                <Components.TabPanel
-                    className="newfold-marketplace-tabs"
-                    activeClass="current-tab"
-                    orientation="vertical"
-                    initialTabName={ initialTab }
-                    onSelect={ onTabNavigate }
-                    tabs={ marketplaceCategories }
-                >
-                    { ( tab ) => <MarketplaceList
-                        marketplaceItems={marketplaceItems}
-                        category={tab.name}
-                        Components={Components}
-                        methods={methods}
-                        constants={constants}
-                        currentCount={tab.currentCount}
-                        saveCategoryDisplayCount={saveCategoryDisplayCount}
-                    /> }
-                </Components.TabPanel>
-            }
-        </div>
-    )
+	return (
+		<div className={methods.classnames('newfold-marketplace-wrapper')}>
+			{ isLoading && 
+				<Components.Spinner />
+			}
+			{ isError && 
+				<h3>Oops, we encountered an error loading the marketplace, please try again later.</h3>
+			}
+			{ !isLoading && !isError &&
+				<Components.TabPanel
+					className="newfold-marketplace-tabs"
+					activeClass="current-tab"
+					orientation="vertical"
+					initialTabName={ initialTab }
+					onSelect={ onTabNavigate }
+					tabs={ marketplaceCategories }
+				>
+					{ ( tab ) => <MarketplaceList
+						marketplaceItems={marketplaceItems}
+						category={tab.name}
+						Components={Components}
+						methods={methods}
+						constants={constants}
+						currentCount={tab.currentCount}
+						saveCategoryDisplayCount={saveCategoryDisplayCount}
+					/> }
+				</Components.TabPanel>
+			}
+		</div>
+	)
 
 };
 
