40550e9e5a868ceb18b9545c82d7eeafac866510
A. Alani <38976631+aalani95@users.noreply.github.com>
2022-11-08 10:29:23 -0700

    WordPress themes related customizations (#21)
    Moves current themes.php page(s) customization in the Bluehost plugin to this module to make it available for all brands.

--- includes/Marketplace.php
+++ includes/Marketplace.php
@@ -24,6 +24,7 @@ class Marketplace {
 
 		// Module functionality goes here
 		add_action( 'rest_api_init', array( MarketplaceApi::class, 'registerRoutes' ) );
+		add_action( 'wp_loaded', array( Themes::class, 'init' ) );
 	}
 
 }
