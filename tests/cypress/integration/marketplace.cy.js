cda01da9e1ee7d38a85ab622e8101a2cfe9b88a9
Evan Mullins <evanrm@gmail.com>
2023-12-13 11:41:29 -0500

    check that screenshots for failed tests are unique per plugin

--- tests/cypress/integration/marketplace.cy.js
+++ tests/cypress/integration/marketplace.cy.js
@@ -146,7 +146,7 @@ describe( 'Marketplace Page', function () {
 	} );
 
 	// CTB Not supported yet on all plugins
-	it.skip( 'Product CTB cards render correctly', () => {
+	it( 'Product CTB cards render correctly', () => {
 		cy.get( appClass + '-app-subnavitem-SEO' ).click();
 		cy.get(
 			'.marketplace-item-a1ff70f1-9670-4e25-a0e1-a068d3e43a45 a.nfd-button'
